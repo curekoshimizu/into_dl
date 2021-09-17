a78163fa5d4ef5199b2d133033bc5e6814e3b97e
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-09-17 22:17:29 +0900

    add sigmoid

--- into_dl/basic_function.py
+++ into_dl/basic_function.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import cast
 
 import numpy
@@ -8,5 +9,12 @@ def step_function(
     x: NDArray[numpy.float64],
 ) -> NDArray[numpy.float64]:
     y = x > 0
-    return cast(NDArray[numpy.float64], y.astype(numpy.int_))
+    return cast(NDArray[numpy.float64], y.astype(numpy.float64))
 
+
+def sigmoid(
+    x: NDArray[numpy.float64],
+) -> NDArray[numpy.float64]:
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore")
+        return 1.0 / (1.0 + numpy.exp(-x))
--- tests/test_basic_function.py
+++ tests/test_basic_function.py
@@ -1,6 +1,6 @@
 import numpy
 
-from into_dl.basic_function import step_function
+from into_dl.basic_function import sigmoid, step_function
 
 
 def test_step_function() -> None:
@@ -8,3 +8,13 @@ def test_step_function() -> None:
     assert numpy.all(y > 0) and y.shape == (2,)
     y = step_function(numpy.array([-1.0, -10.0, 0.0]))
     assert numpy.all(y == 0) and y.shape == (3,)
+
+
+def test_sigmoid() -> None:
+    y = sigmoid(numpy.array([0.0]))
+    assert y == numpy.array([0.5])
+
+    z = 1.0e5
+    y = sigmoid(numpy.array([z, -z]))
+    assert 0.9 < y[0] <= 1.0
+    assert 0 <= y[1] < 0.1
